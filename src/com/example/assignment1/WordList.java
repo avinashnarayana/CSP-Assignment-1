0d9eba18c82288c38d642395d8fb304ded9c0407
user <avinash.narayana@gmail.com>
2016-04-06 05:31:57 +0100

    Task 3

---
+++ src/com/example/assignment1/WordList.java
@@ -0,0 +1,26 @@
+// WordList.java 
+// 
+// Simple demonstration of a JDO annotated class that will store a user key and 
+// store a list of words in it
+package com.example.assignment1;
+//imports 
+import java.util.ArrayList;
+
+import javax.jdo.annotations.PersistenceCapable; 
+import javax.jdo.annotations.Persistent; 
+import javax.jdo.annotations.PrimaryKey;
+
+import com.google.appengine.api.datastore.Key;
+// class definition 
+@PersistenceCapable 
+public class WordList { 
+	// the identifier for the object as we will be using the user key for this 
+	// we need to use a key object here 
+	@PrimaryKey 
+	@Persistent 
+	private Key id;
+	// the list of words
+	@Persistent 
+	private ArrayList<String> words;
+	
+}
