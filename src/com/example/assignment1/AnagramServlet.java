5fc1a6e4a21c68fe1fa6fcca60939b20937970d4
user <avinash.narayana@gmail.com>
2016-04-08 05:47:46 +0100

    Task 10

--- src/com/example/assignment1/AnagramServlet.java
+++ src/com/example/assignment1/AnagramServlet.java
@@ -17,28 +17,7 @@ import com.google.appengine.api.users.UserServiceFactory;
 public class AnagramServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException, ServletException {
-		resp.setContentType("text/plain");
-		// we need to get access to the google user service 
-		UserService us = UserServiceFactory.getUserService(); 
-		User u = us.getCurrentUser(); 
-		String login_url = us.createLoginURL("/"); 
-		String logout_url = us.createLogoutURL("/");
-		// persistence manager and a user we declare them like this because 
-		// each time we close we need a new persistence manager for the following 
-		// query 
-		PersistenceManager pm = null; 
-		// get access to the user. if they do not exist in the datastore then 
-		// store a default version of them. of course we have to check that a user has 
-		// logged in first 
-		
-			pm = PMF.get().getPersistenceManager();
-			
-		
-		// attach a few things to the request such that we can access them in the jsp 
-		req.setAttribute("user", u); 
-		req.setAttribute("login_url", login_url); 
-		req.setAttribute("logout_url", logout_url);
-		
+		RootServlet.userLogin(req,resp);
 		
 		//check whether anything was input by user
 		String search = req.getParameter("check_anagram");
@@ -47,25 +26,35 @@ public class AnagramServlet extends HttpServlet {
 			req.setAttribute("errMsg", "Input string is empty");
 			validWord = false;
 		}
-		// get access to a request dispatcher and forward onto the root.jsp file
+		
+		// Associating each letter to a prime number, a unique integer can be generated
+		// for a particular letter combination by multiplying all the primes together.
+		// We calculate that unique ID for the search string here
 		int[] primes = {2,3,5,7,11,
 				13,17,19,23,29,
 				31,37,41,43,47,
 				53,59,61,67,71,
 				73,79,83,89,97,101};
-		// Associating each letter to a prime number, a unique integer can be generated
-		// for a particular letter combination
+		
 		long uniqueID = 1;
+		
 		try{// surrounding with try catch to deal with index out of bounds exception
-			// when a white space of other character apart from alphabet is entered
-		for(int i=0;i<search.length();i++){
-			int ascii=search.charAt(i);
-			if(ascii>96)ascii-=96;
-			else ascii-=64;
-			uniqueID*=primes[ascii-1];
-		}}catch(Exception e){req.setAttribute("errMsg", "Input word is invalid");validWord=false;}
+			// when a non-alphabet character is entered
+			for(int i=0;i<search.length();i++){
+				int ascii=search.charAt(i);
+				// converting the ascii of the character into an index of 0-25
+				if(ascii>96)ascii-=97; //for lower-case
+				else ascii-=65; // for upper-cased letters
+				uniqueID*=primes[ascii];
+		}}catch(IndexOutOfBoundsException e){
+			req.setAttribute("errMsg", "Input word is invalid");
+			validWord=false;
+		}
+		
+		// Process it if it is a valid word
 		if(validWord){
 			Key word_key = KeyFactory.createKey("WordList", uniqueID);
+			PersistenceManager pm = PMF.get().getPersistenceManager();
 			try { 
 				WordList resultList = pm.getObjectById(WordList.class, word_key);
 				
@@ -83,8 +72,8 @@ public class AnagramServlet extends HttpServlet {
 				req.setAttribute("errMsg", "No anagrams found");
 				req.setAttribute("displayMsg", search+" was added to dictionary");
 			}
+			pm.close(); 
 		}
-		pm.close(); 
 		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/root.jsp"); 
 		rd.forward(req, resp);
 	}
@@ -92,32 +81,60 @@ public class AnagramServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		/*
-		// get the new timezone from the form 
-		int timezone = 0; 
-		try{ 
-			timezone = Integer.parseInt(req.getParameter("new_timezone")); 
-		} catch(NumberFormatException e) { 
-			// failed to update so just redirect to / 
-			resp.sendRedirect("/"); 
+		RootServlet.userLogin(req,resp);
+		//check whether anything was input by user
+		String search = req.getParameter("submit_word");
+		boolean validWord = true;
+		if(search==null||search.equals("")){
+			req.setAttribute("errMsg", "Input string is empty");
+			validWord = false;
 		}
 		
-		// get access to the user service to get our user 
-		UserService us = UserServiceFactory.getUserService(); 
-		User u = us.getCurrentUser();
-		// update the timezone in the datastore and then redirect to / 
-		PersistenceManager pm = null; 
-		ClockUser settings; 
-		Key user_key = KeyFactory.createKey("ClockUser", u.getUserId()); 
-		try { 
-			pm = PMF.get().getPersistenceManager(); 
-			settings = pm.getObjectById(ClockUser.class, user_key); 
-			settings.setOffset(timezone); pm.makePersistent(settings); 
-		} catch (Exception e) { 
-			// will only fail if the datastore goes down as this is 
-			// already in the datastore 
+		// Associating each letter to a prime number, a unique integer can be generated
+		// for a particular letter combination by multiplying all the primes together.
+		// We calculate that unique ID for the search string here
+		int[] primes = {2,3,5,7,11,
+				13,17,19,23,29,
+				31,37,41,43,47,
+				53,59,61,67,71,
+				73,79,83,89,97,101};
+		
+		long uniqueID = 1;
+		try{// surrounding with try catch to deal with index out of bounds exception
+			// when a non-alphabet character is entered
+			for(int i=0;i<search.length();i++){
+				int ascii=search.charAt(i);
+				// converting the ascii of the character into an index of 0-25
+				if(ascii>96)ascii-=97; //for lower-case
+				else ascii-=65; // for upper-cased letters
+				uniqueID*=primes[ascii];
+		}}catch(IndexOutOfBoundsException e){
+			req.setAttribute("errMsg", "Input word is invalid");
+			validWord=false;
 		}
-		pm.close();*/ 
-		resp.sendRedirect("/");
+		
+		// Process it if it is a valid word
+		if(validWord){
+			PersistenceManager pm = PMF.get().getPersistenceManager();
+			Key word_key = KeyFactory.createKey("WordList", uniqueID);
+			try { 
+				WordList resultList = pm.getObjectById(WordList.class, word_key);
+				
+				if(!resultList.contains(search)){
+					resultList.addWord(search);
+					req.setAttribute("displayMsg", search+" was added to dictionary");
+				}
+				else
+					req.setAttribute("displayMsg", search+" already in dictionary");
+			} catch(Exception e) {
+				WordList w = new WordList(word_key);
+				w.addWord(search);
+				pm.makePersistent(w);
+				req.setAttribute("displayMsg", search+" was added to dictionary");
+			}
+			pm.close();
+		}
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/root.jsp"); 
+		rd.forward(req, resp);
 	}
 }
--- src/com/example/assignment1/RootServlet.java
+++ src/com/example/assignment1/RootServlet.java
@@ -1,11 +1,6 @@
 package com.example.assignment1;
 
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.SimpleTimeZone;
-
-import javax.jdo.PersistenceManager;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
@@ -18,29 +13,23 @@ import com.google.appengine.api.users.UserServiceFactory;
 public class RootServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException, ServletException {
+		userLogin(req,resp);
+		// get access to a request dispatcher and forward onto the root.jsp file 
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/root.jsp"); 
+		rd.forward(req, resp);
+	}
+	// A utility method to check whether the user is logged in
+	public static void userLogin(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException, ServletException {
 		resp.setContentType("text/plain");
 		// we need to get access to the google user service 
 		UserService us = UserServiceFactory.getUserService(); 
 		User u = us.getCurrentUser(); 
 		String login_url = us.createLoginURL("/"); 
 		String logout_url = us.createLogoutURL("/");
-		// persistence manager and a user we declare them like this because 
-		// each time we close we need a new persistence manager for the following 
-		// query 
-		PersistenceManager pm = null; 
-		// get access to the user. if they do not exist in the datastore then 
-		// store a default version of them. of course we have to check that a user has 
-		// logged in first 
-		if(u != null) { 
-			pm = PMF.get().getPersistenceManager();
-			pm.close(); 
-		}
 		// attach a few things to the request such that we can access them in the jsp 
 		req.setAttribute("user", u); 
 		req.setAttribute("login_url", login_url); 
 		req.setAttribute("logout_url", logout_url);
-		// get access to a request dispatcher and forward onto the root.jsp file 
-		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/root.jsp"); 
-		rd.forward(req, resp);
 	}
 }
